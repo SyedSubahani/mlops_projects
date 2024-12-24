85f26adb566b29f65c2aa0994438721e7754787d
SyedSubahani <syed.subahani@gmail.com>
2024-12-24 13:38:36 +0530

    Add Logging module

---
+++ gemstone-price-prediction-MLOPS/src/logger/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+logFile=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logPath=os.path.join(os.getcwd(),"logs")
+
+os.makedirs(logPath,exist_ok=True)
+
+logFilePath=os.path.join(logPath,logFile)
+
+
+logging.basicConfig(level=logging.INFO, 
+                    filename=logFilePath,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+                    
+)
